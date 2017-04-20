3a171f8994c713425bbdfa2a9475e4ea0907582f
Franklin Ross <fhross@gmail.com>
2017-04-21 01:29:40 +1000

    Changed babel preset to node6.

--- dist/rollup-plugin-extension-mapper.js
+++ dist/rollup-plugin-extension-mapper.js
@@ -1,12 +1,23 @@
-import path from 'path';
+"use strict";
 
-export default function plugin(options = {}) {
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = plugin;
+
+var _path = require("path");
+
+var _path2 = _interopRequireDefault(_path);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function plugin(options = {}) {
 
   //List of transform functions to try for each path. The first which returns a value wins.
   const transformers = [];
 
   //Ensures the provided string begins with a period and adds one if not.
-  const ensureHasLeadingDot = (ext) => ext.startsWith(".") ? ext : ("." + ext);
+  const ensureHasLeadingDot = ext => ext.startsWith(".") ? ext : "." + ext;
 
   //Builds a list of transform functions.
   for (let extension in options) if (options.hasOwnProperty(extension)) {
@@ -20,9 +31,9 @@ export default function plugin(options = {}) {
       const oldExtension = extension;
       transform = (importee, importer) => {
         const newFileName = importee.substr(0, importee.length - oldExtension.length) + newExtension;
-        const importDir = path.dirname(importer);
-        return path.resolve(importDir, newFileName);
-      }
+        const importDir = _path2.default.dirname(importer);
+        return _path2.default.resolve(importDir, newFileName);
+      };
     } else {
       console.warn(`rollup-plugin-extension-mapper expects the value of options.${extension} to be a string or a function`);
       continue;
